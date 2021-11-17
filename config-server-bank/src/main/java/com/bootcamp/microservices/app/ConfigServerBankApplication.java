c65c6cf288495922c8c40c4b62f95fea6aa8afaf
allanosd <93173159+allanosd@users.noreply.github.com>
2021-11-17 16:33:40 -0500

    updated

---
+++ config-server-bank/src/main/java/com/bootcamp/microservices/app/ConfigServerBankApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.microservices.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerBankApplication.class, args);
+	}
+
+}
